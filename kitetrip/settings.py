deb06dc990c83619c9ed3808364ce9554f87ddd4
NicolasDutour <dutourn99@gmail.com>
2019-09-04 13:24:37 +0200

    Connect to DataBase

--- kitetrip/settings.py
+++ kitetrip/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'kitetrip.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kitetrip',
+        'USER': 'postgres',
+        'PASSWORD': 'Senegal1983',
+        'HOST': 'localhost'
     }
 }
 
