7f835a4db76ae38e2535c3593d1cf16788b420d4
NicolasDutour <dutourn99@gmail.com>
2019-09-05 09:42:25 +0200

    Add handleChange function in Login component

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,19 +6,36 @@ import '../assets/css/loginAndSignup.scss'
 
 class Login extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+handleChange = e => {
+  this.setState({
+    [e.target.name]: e.target.value
+  })
+}
+
+
   render() {
+
+    const { email, password } = this.state
 
     return (
       <section id="login" className="container">
         <div className="row align-items-center justify-content-center">
           <div className="col-md-5 login-form">
-            <h3 className="title"><i class="fas fa-sign-in-alt"></i> Se Connecter</h3>
+            <h3 className="title"><i className="fas fa-sign-in-alt"></i> Se Connecter</h3>
             <form>
               <div className="form-group">
-                <input name="email" type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" />
+                <input name="email" type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Email" value={email} onChange={this.handleChange} />
               </div>
               <div className="form-group">
-                <input name="password" type="password" className="form-control" id="password" placeholder="Mot de passe" />
+                <input name="password" type="password" className="form-control" id="password" placeholder="Mot de passe" value={password} onChange={this.handleChange} />
               </div>
               <button type="submit" className="btn btn-primary">Se Connecter</button>
             </form>
