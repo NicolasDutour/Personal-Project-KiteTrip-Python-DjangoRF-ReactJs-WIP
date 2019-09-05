55037df8b60738009a968cf571b6ddee160ea361
NicolasDutour <dutourn99@gmail.com>
2019-09-05 14:56:09 +0200

    Add expirationDate for token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,6 +8,12 @@ export const authSuccess = token => {
   }
 }
 
+export const authFail = () => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+  }
+}
+
 
 export const logout = () => {
   localStorage.removeItem("token")
@@ -15,6 +21,14 @@ export const logout = () => {
     type: actionTypes.AUTH_LOGOUT
   }
 }
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
 
 export const authLogin = (email, password) => {
   return dispatch => {
@@ -26,11 +40,15 @@ export const authLogin = (email, password) => {
       .then(res => {
         console.log(res.data)
         const token = res.data.key
+        const expirationDate = new Date(new Date().getTime() + 3600 * 1000);
         localStorage.setItem('token', token)
+        localStorage.setItem("expirationDate", expirationDate);
         dispatch(authSuccess(token))
+        dispatch(checkAuthTimeout(3600));
       })
       .catch(err => {
         console.log(err.response)
+        dispatch(authFail())
       })
   }
 }
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -21,12 +21,22 @@ const authLogout = (state, action) => {
   }
 }
 
+const authFail = (state, action) => {
+  return {
+    ...state,
+    token: null,
+    isAuthenticated: false
+  }
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_SUCCESS:
       return authSuccess(state, action)
     case actionTypes.AUTH_LOGOUT:
       return authLogout(state, action)
+    case actionTypes.AUTH_FAIL:
+      return authFail(state, action)
     default:
       return state;
   }
